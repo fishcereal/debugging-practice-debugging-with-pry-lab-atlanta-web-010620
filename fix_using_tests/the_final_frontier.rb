9fb2ab9522158af3aca330433f81d4b007c38200
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-12 16:24:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -17,8 +17,8 @@ end
 
 def engage
   date = generate_star_date
-  state_log(date)
   greet_crew(creww)
+  state_log(date)
 end
 
 #     expect(engage).to eq(["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."])
